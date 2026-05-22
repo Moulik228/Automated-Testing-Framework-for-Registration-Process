840d43ca7aff665c86b5d3f930f3d420cdbbdbf0
Moulik228 <majimoulik@gmail.com>
2026-05-22 21:50:03 +0530

    Add files via upload

---
+++ TestListener.java
@@ -0,0 +1,22 @@
+package Registration;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestListener implements ITestListener {
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        System.out.println("Test Started: " + result.getMethod().getMethodName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        System.out.println("Test Passed: " + result.getMethod().getMethodName());
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        System.out.println("Test Failed: " + result.getMethod().getMethodName());
+    }
+}
